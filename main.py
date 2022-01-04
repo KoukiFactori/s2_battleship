44eb7b036ba28eb2efd46e155d26b26399384f0e
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2022-01-04 03:09:05 +0100

    Suppression des messages automatiques de jetbrain

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pygame
 
 from view import window
@@ -22,5 +17,3 @@ def main_test():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main_test()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
