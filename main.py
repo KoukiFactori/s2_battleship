d7a5a0a74765490aa60f5867338a7672781c0b7a
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2022-01-05 16:40:25 +0100

    Ecriture de la fonction placerBateauxManuel

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from view import window
 from model.Constantes import *
 from model.Joueur import construireJoueur
 
+from model.Manuel import placerBateauxManuel
+
 def main_test():
-    j = construireJoueur("Test", [const.PORTE_AVION, const.CUIRASSE, const.CROISEUR, const.TORPILLEUR])
+    j = construireJoueur("Teiuwu", [const.PORTE_AVION, const.CUIRASSE, const.CROISEUR, const.TORPILLEUR])
     # j = construireJoueur("Test", [const.PORTE_AVION, const.CUIRASSE])
-    window.afficher(j)
-    window.placer_bateaux()
+    
+    placerBateauxManuel(j)
     window.set_action("Pour terminer, cliquez dans la grille de DROITE")
     window.get_clicked_cell(2)
 
--- model/Manuel.py
+++ model/Manuel.py
@@ -1,3 +1,16 @@
 # model/Manuel.py
 #
 
+from view import window
+from model.Joueur import getNomJoueur, type_joueur
+
+def placerBateauxManuel(joueur: dict) -> None:
+
+  if not type_joueur(joueur):
+    raise ValueError("placerBateauxManuel : Le joueur {joueur} n'est pas un joueur valide")
+
+  window.afficher(joueur)
+  window.display_message(f"{getNomJoueur(joueur)} : Placez vos bateaux")
+  window.placer_bateaux()
+
+  return None
