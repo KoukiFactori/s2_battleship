5dc9d0967148a9883748ab40f2f4027609ec4748
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2022-01-06 03:48:05 +0100

    Update main

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import pygame
 from view import window
 
 from model.Constantes import *
-from model.Joueur import construireJoueur
+from model.Joueur import construireJoueur, repondreTirJoueur
 
 from model.Manuel import placerBateauxManuel, choisirCaseTireManuel
 
 def main_test():
-    j = construireJoueur("Teiuwu", [const.PORTE_AVION, const.CUIRASSE, const.CROISEUR, const.TORPILLEUR])
-    # j = construireJoueur("Test", [const.PORTE_AVION, const.CUIRASSE])
+    #j = construireJoueur("Teiuwu", [const.PORTE_AVION, const.CUIRASSE, const.CROISEUR, const.TORPILLEUR])
+    j = construireJoueur("Teiuwu", [const.PORTE_AVION, const.CUIRASSE])
     
     placerBateauxManuel(j)
     window.set_action("Pour terminer, cliquez dans la grille de DROITE")
     window.get_clicked_cell(2)
-    choisirCaseTireManuel(j)
+    while True:
+        x = choisirCaseTireManuel(j)
+        res = repondreTirJoueur(j, x)
+        print(res)
+        window.refresh()
+    window.set_action("Pour terminer, cliquez dans la grille de DROITE") 
+    window.get_clicked_cell(2)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
